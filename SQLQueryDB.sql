f37dd9fedb4a10fa5a41d1a638bf80f02065feba
EshaDevre <85304584+EshaDevre@users.noreply.github.com>
2022-01-10 19:09:49 +0530

    Add files via upload

---
+++ SQLQueryDB.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE DB;
+Use DB;
+CREATE TABLE Stu(
+id int PRIMARY KEY,
+name varchar(20),
+age int DEFAULT 18
+);
+
+alter table Stu
+add gender varchar(20);
+alter table Stu
+drop column gender;
+ALTER TABLE Stu
+ADD email varchar(50);
+
+INSERT INTO Stu
+VALUES(1,'Esha',21,'esha.devre@aimdek.com'),
+(2,'Jagruti',22,'jagruti.suthar@aimdek.com'),
+(3,'Jeel',23,'jeel.prajapati@aimdek.com');
+
+Insert into stu(id,name,age)
+values(5,'abc',13)
+
+select * from Stu
+where age=19
+
+select * from Stu
+order by age
+
+select * from Stu
+order by age desc
+
+select * from Stu
+where age=19 or age=18
+
+select * from Stu
+where email like '%aimdek.com';
+
+select * from Stu
+where age like '1_';
+
+Select count(*), age from stu group by age
+
+select distinct age from stu
