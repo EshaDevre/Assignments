dd82719ed57eca00bf40df4c28ea088fddbb13d1
EshaDevre <85304584+EshaDevre@users.noreply.github.com>
2022-01-11 18:24:27 +0530

    Update SQLQueryProcedure.sql

--- SQLQueryProcedure.sql
+++ SQLQueryProcedure.sql
@@ -41,3 +41,22 @@
 
  drop view stud;
  drop view stu;
+ 
+ 
+  /* DIFFERENCE BETWEEN VIEW AND SHARED STORAGE */
+  
+  A Stored Procedure:
+
+Accepts parameters
+Can NOT be used as building block in a larger query
+Can contain several statements, loops, IF ELSE, etc.
+Can perform modifications to one or several tables
+Can NOT be used as the target of an INSERT, UPDATE or DELETE statement.
+
+A View:
+
+Does NOT accept parameters
+Can be used as building block in a larger query
+Can contain only one single SELECT query
+Can NOT perform modifications to any table
+But can (sometimes) be used as the target of an INSERT, UPDATE or DELETE statement.
